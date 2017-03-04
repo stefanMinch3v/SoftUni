c99e3d41347f275c9f87e1f92aecd55984092a07
stefan <amxx@abv.bg>
2017-03-04 09:18:54 +0100

    concat data code

---
+++ C#BasicsMarch2015/2.SimpleCalculations/ConcatenateData/ConcatenateData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConcatenateData
+{
+    class ConcatenateData
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, please enter your details!");
+            Console.Write("Your first name: ");
+            string FirstName = Console.ReadLine();
+            Console.Write("Your last name: ");
+            string LastName = Console.ReadLine();
+            Console.Write("Your age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Your town: ");
+            string town = Console.ReadLine();
+            Console.WriteLine("You are {0} {1}, a {2} -years old person from {3}.", FirstName, LastName, age, town);
+        }
+    }
+}
