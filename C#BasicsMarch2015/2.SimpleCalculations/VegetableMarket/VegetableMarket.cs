7ad0125de1565103e60d1799e40946ae5966bce0
stefan <amxx@abv.bg>
2017-03-04 14:52:30 +0100

    more complicated exc - vegetable market

---
+++ C#BasicsMarch2015/2.SimpleCalculations/VegetableMarket/VegetableMarket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VegetableMarket
+{
+    class VegetableMarket
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Price for 1kg vegetables : ");
+            var vegetable = double.Parse(Console.ReadLine());
+            Console.Write("Price for 1kg fruit : ");
+            var fruit = double.Parse(Console.ReadLine());
+            Console.Write("How many kg for vegetables : ");
+            var kg = int.Parse(Console.ReadLine());
+            Console.Write("How many kg for fruit : ");
+            var kg2 = int.Parse(Console.ReadLine());
+            double result = (vegetable * kg + fruit * kg) / 1.94f;
+            Console.WriteLine("All together are : " + result + " Euro");
+
+        }
+    }
+}
