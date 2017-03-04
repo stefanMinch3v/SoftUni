84817610dde3dd1ff4e0370726431c1867ab5865
stefan <amxx@abv.bg>
2017-03-04 09:17:21 +0100

    circle area code

---
+++ C#BasicsMarch2015/2.SimpleCalculations/CircleAreanAndPerimeter/CircleAreaAndPerimeter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleAreanAndPerimeter
+{
+    class CircleAreaAndPerimeter
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number to try : ");
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * r * r;
+            double perimeter = 2 * Math.PI * r;
+            Console.WriteLine("Area = " + area);
+            Console.WriteLine("Perimeter = " + perimeter);
+        }
+    }
+}
