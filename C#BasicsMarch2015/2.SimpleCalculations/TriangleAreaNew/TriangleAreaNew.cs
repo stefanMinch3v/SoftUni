bcb80dd2b3612b94fb20f827bddd8b3ca082801f
stefan <amxx@abv.bg>
2017-03-04 14:43:41 +0100

    triangle area

---
+++ C#BasicsMarch2015/2.SimpleCalculations/TriangleAreaNew/TriangleAreaNew.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaNew
+{
+    class TriangleAreaNew
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Height = ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("Width = ");
+            double width = double.Parse(Console.ReadLine());
+            double area = width * height / 2;
+            Console.WriteLine("Triangle area = " + Math.Round(area, 2));
+        }
+    }
+}
