ce6c315dcbd5b84b3d4c476c11444e473c2a7656
stefan <amxx@abv.bg>
2017-03-04 21:44:05 +0100

    even or odd num

---
+++ C#BasicsMarch2015/3.SimpleConditions/EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class EvenOrOdd
+    {
+        static void Main(string[] args)
+        {
+            var num = double.Parse(Console.ReadLine());
+            if(num % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
