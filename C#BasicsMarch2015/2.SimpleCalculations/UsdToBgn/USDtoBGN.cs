4efc2ddcd95a3b1cac8c5aee528353b5e2bcac41
stefan <amxx@abv.bg>
2017-03-04 14:45:01 +0100

    convert usd to bgn

---
+++ C#BasicsMarch2015/2.SimpleCalculations/UsdToBgn/USDtoBGN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsdToBgn
+{
+    class USDtoBGN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("USD = ");
+            double usd = double.Parse(Console.ReadLine());
+            double convert = usd * 1.79549f;
+            Console.WriteLine("BGN = {0}", Math.Round(convert, 2));
+        }
+    }
+}
