a962d6267c99a139e1e38a736dc92759f83b6684
stefan <amxx@abv.bg>
2017-03-04 12:00:45 +0100

    trapezoid area, two ways

---
+++ C#BasicsMarch2015/2.SimpleCalculations/Trapezoid-Area/SomeExercises.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int ii = i; ii <= n; ii++)
+                {
+                    Console.Write(ii);
+                }
+                Console.WriteLine();
+            }*/
+            var n = int.Parse(Console.ReadLine());
+            Console.Write("+ ");
+            for (var i = 1; i <= n - 2; i++)
+            {
+                Console.Write("- ");
+            }
+            Console.WriteLine("+");
+            for (var ii = 1; ii <= n - 2; ii++)
+            {
+                Console.Write("| ");
+                for (int iii = 1; iii <= n - 2; iii++)
+                {
+                    Console.Write("- ");
+                }
+                Console.WriteLine("|");
+            }
+            Console.Write("+ ");
+            for (var iiii = 1; iiii <= n - 2; iiii++)
+            {
+                Console.Write("- ");
+            }
+            Console.WriteLine("+");
+        }
+    }
+}
---
+++ C#BasicsMarch2015/2.SimpleCalculations/TrapezoidArea/TrapezoidArea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrapezoidArea
+{
+    class TrapezoidArea
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the unique formula of the trapezoid area! Please enter the details to calculate");
+            Console.Write("The one side of the trapezoid: ");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.Write("The other side of the trapezoid: ");
+            double b2 = double.Parse(Console.ReadLine());
+            Console.Write("The height of the trapezoid: ");
+            double h = double.Parse(Console.ReadLine());
+            double area = (b1 + b2) * h / 2;
+            Console.WriteLine("Trapezoid area = " + area); 
+        }
+    }
+}
