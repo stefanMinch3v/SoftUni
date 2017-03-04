6dcf9778f59a3009011d8e0bb36e37467ef959a2
stefan <amxx@abv.bg>
2017-03-04 11:56:59 +0100

    convert radian to degree

---
+++ C#BasicsMarch2015/2.SimpleCalculations/RadianToDegree/RadiansToDegrees.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadianToDegree
+{
+    class RadiansToDegrees
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Radians = ");
+            double radians = double.Parse(Console.ReadLine());
+            double calculate =  180 / Math.PI * radians;
+            Console.WriteLine("Degree = {0}",Math.Round(calculate,1));
+        }
+    }
+}
