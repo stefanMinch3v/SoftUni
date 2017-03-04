6204fe8372081783503aa5cd0045889ed7cfb6bb
stefan <amxx@abv.bg>
2017-03-04 11:59:02 +0100

    square area code

---
+++ C#BasicsMarch2015/2.SimpleCalculations/SquareArea/SquareArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a = ");
+            int a = int.Parse(Console.ReadLine()); //var a = int.Parse(Console.ReadLine());
+            int area = a * a; // var area = a * a;
+            Console.Write("Square = ");
+            Console.WriteLine(area);
+        }
+    }
+}
