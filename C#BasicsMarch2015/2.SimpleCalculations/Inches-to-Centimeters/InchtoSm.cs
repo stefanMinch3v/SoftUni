09f7b484360341af2fc3c5b93194d347e05ad7cb
stefan <amxx@abv.bg>
2017-03-04 11:05:06 +0100

    convert inch to cm

---
+++ C#BasicsMarch2015/2.SimpleCalculations/Inches-to-Centimeters/InchtoSm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Inches = ");
+            double inch = double.Parse(Console.ReadLine());
+            double sm = inch * 2.54f;
+            Console.Write("Sentimeters = ");
+            Console.WriteLine(sm);
+        }
+    }
+}
