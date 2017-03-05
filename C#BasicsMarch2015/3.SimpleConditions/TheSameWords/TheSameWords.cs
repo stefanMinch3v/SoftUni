88894dc4baabe57cee872a113ca3985f0eace1a5
stefan <amxx@abv.bg>
2017-03-05 23:08:00 +0100

    same words

---
+++ C#BasicsMarch2015/3.SimpleConditions/TheSameWords/TheSameWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSameWords
+{
+    class TheSameWords
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some word: ");
+            var word = Console.ReadLine();
+            word = word.ToLower();
+            Console.Write("Enter another word: ");
+            var word2 = Console.ReadLine();
+            word2 = word2.ToLower();
+            if (word == word2)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
