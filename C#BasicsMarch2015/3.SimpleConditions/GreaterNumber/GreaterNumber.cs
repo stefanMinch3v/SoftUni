e17bfb13909e5d4006ef55f98d2aa401e3b834fc
stefan <amxx@abv.bg>
2017-03-05 09:23:14 +0100

    greater number

---
+++ C#BasicsMarch2015/3.SimpleConditions/GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreaterNumber
+{
+    class GreaterNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number :");
+            var num = int.Parse(Console.ReadLine());
+            Console.Write("Enter number2 :");
+            var num2 = int.Parse(Console.ReadLine());
+            if (num > num2)
+            {
+                Console.WriteLine("Greater number is :" + num);
+            }
+            else
+            {
+                Console.WriteLine("Greater number is :" + num2);
+            }
+        }
+    }
+}
