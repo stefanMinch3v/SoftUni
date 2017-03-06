449e491f7118786ee589dbd0214c5af388c3164f
stefan <amxx@abv.bg>
2017-03-06 13:58:05 +0100

    exercises about fruit and fr shop

---
+++ C#BasicsMarch2015/4.ComplexConditions/FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitOrVegetable
+{
+    class FruitOrVegetable
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter fruit or vegetable: ");
+            var kind = Console.ReadLine().ToLower();
+            if(kind == "banana" || kind == "apple" || kind == "kiwi" || kind == "cherry" || kind == "lemon" || kind == "grapes")
+            {
+                Console.WriteLine("Fruit");
+            }
+            else if(kind == "tomato" || kind == "cucumber" || kind == "pepper" || kind == "carrot")
+            {
+                Console.WriteLine("Vegetable");
+            }
+            else
+            {
+                Console.WriteLine("Unknown");
+            }
+        }
+    }
+}
---
+++ C#BasicsMarch2015/4.ComplexConditions/FruitShop/FruitShop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitShop
+{
+    class FruitShop
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter some fruit: ");
+            var fruit = Console.ReadLine().ToLower();
+            Console.Write("Enter day of the week: ");
+            var day = Console.ReadLine().ToLower();
+            Console.Write("Enter quantity: ");
+            var quantity = double.Parse(Console.ReadLine());
+            var price = -1.0;
+            if (day == "monday" || day == "tuesday" || day == "wednesday" || day == "thirdsday" || day == "friday")
+            {
+                if (fruit == "banana") price = 2.50; 
+                else if (fruit == "apple") price = 1.20;
+                else if (fruit == "orange") price = 0.85;
+                else if (fruit == "grapefruit") price = 1.45;
+                else if (fruit == "kiwi") price = 2.70;
+                else if (fruit == "pineapple") price = 5.50;
+                else if (fruit == "grapes") price = 3.85;
+            }
+            else if (day == "saturday" || day == "sunday")
+            {
+                if (fruit == "banana") price = 2.70;
+                else if (fruit == "apple") price = 1.25;
+                else if (fruit == "orange") price = 0.90;
+                else if (fruit == "grapefruit") price = 1.60;
+                else if (fruit == "kiwi") price = 3.00;
+                else if (fruit == "pineapple") price = 5.60;
+                else if (fruit == "grapes") price = 4.20;
+            }
+
+            if(price >= 0)
+            {
+                Console.WriteLine("{0:f2}", price * quantity);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
