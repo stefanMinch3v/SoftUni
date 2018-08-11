86e4b2117e7552ef2d29c4758da99ff4b3a99f44
stefan <amxx@abv.bg>
2018-08-11 16:11:48 +0200

    Per type architecture for Nodejs, Express, Mongoose and Handlebars from softuni and slightly modified by my taste

---
+++ Express-project-template/server/config/settings.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+let rootPath = path.normalize(path.join(__dirname, '/../../'));
+
+// Environment config
+module.exports = {
+    development: {
+        rootPath: rootPath,
+        db: 'mongodb://localhost:27017/generictemplate',
+        port: 1337
+    },
+    staging: {
+    },
+    production: {
+        port: process.env.PORT
+    }
+};
